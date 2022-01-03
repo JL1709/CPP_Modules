91aaeb868cc4b5b377bfe229afa760356c97f214
JL1709 <julian.lu@web.de>
2022-01-03 19:59:56 +0100

    ex00

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -6,8 +6,25 @@
 /*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 09:04:50 by julian            #+#    #+#             */
-/*   Updated: 2021/12/29 09:05:00 by julian           ###   ########.fr       */
+/*   Updated: 2022/01/03 19:51:49 by julian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+
+template< typename T >							// Template definition
+int easyfind(T &container, int const &value)	// Template function
+{
+	typename T::iterator result = std::find(begin(container), end(container), value);
+
+	if (result != std::end(container))
+	{
+		std::cout << "container contains " << value << std::endl;
+		return (1);
+	}
+	else
+		std::cout << "container does not contain " << value << '\n';
+	return (-1);
+}
--- CPP_Module_08/ex00/main.cpp
+++ CPP_Module_08/ex00/main.cpp
@@ -6,15 +6,33 @@
 /*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 09:04:48 by julian            #+#    #+#             */
-/*   Updated: 2021/12/29 09:05:21 by julian           ###   ########.fr       */
+/*   Updated: 2022/01/03 19:58:35 by julian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <array>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include "easyfind.hpp"
 
 int	main(void)
 {
 	
-	
-	
-	
+	std::array<int, 42>	myArray;				// Declare an integer array with length 42
+	std::vector<int>	myVector;
+	std::list<int>		myList;
+
+	for (int i = 0; i < 42; i++)				// Fill array with value
+		std::fill_n (myArray.begin()+i, 1, i);
+	for (int i = 0; i < 42; i++)				// Add new element at the end of the vector
+		myVector.push_back(i);
+	for (int i = 0; i < 42; i++)				// Add new element at the end of the list
+		myList.push_back(i);
+
+	easyfind(myArray, 21);
+	easyfind(myVector, 42);
+	easyfind(myList, 21);
+
 	return (0);
 }
