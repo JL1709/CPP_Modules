d8ca202f2cd1fa2b4bf47ae4d1ff1656aa368450
Julian Ludt <jludt@2-E-7.42heilbronn.de>
2022-02-08 12:59:14 +0100

    07

--- CPP_Module_07/ex00/main.cpp
+++ CPP_Module_07/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jludt <jludt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 17:31:05 by julian            #+#    #+#             */
-/*   Updated: 2021/12/22 18:06:50 by julian           ###   ########.fr       */
+/*   Updated: 2022/02/08 12:57:44 by jludt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,16 @@ int main( void )
 	
 	::swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
 	
 	std::string c = "chaine1";
 	std::string d = "chaine2";
 	
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
 	
 	return 0;
 }
--- CPP_Module_07/ex00/whatever.hpp
+++ CPP_Module_07/ex00/whatever.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julian <julian@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jludt <jludt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/22 17:34:08 by julian            #+#    #+#             */
-/*   Updated: 2022/01/11 10:57:05 by julian           ###   ########.fr       */
+/*   Updated: 2022/02/08 12:51:05 by jludt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 template<typename T>
 void	swap(T &x, T &y)
 {
-	T	tmp = x
+	T tmp(x);
 	x = y;
 	y = tmp;
 }
 
 template<typename T>
-T &min(T const &x, T const &y)
+const T &min(T const &x, T const &y)
 {
 	return (x < y ? x : y);
 }
 
 template<typename T>
-T &max(T const &x, T const &y)
+const T &max(T const &x, T const &y)
 {
 	return (x > y ? x : y);
 }
